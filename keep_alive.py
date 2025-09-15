51d66661eafa669fc92f004881661fec2b0fa1da
LoganSanz <114677579+M0B3@users.noreply.github.com>
2025-09-15 14:28:23 +0200

    Rework for web Pages

--- keep_alive.py
+++ keep_alive.py
@@ -8,7 +8,7 @@ def home():
     return "I'm alive"
 
 def run():
-    app.run(host='0.0.0.0',port=8081) # Run the server on port 8081
+    app.run(host='0.0.0.0',port=8060) # Run the server on port 8081
 
 def keep_alive(): # Start the server in a new thread
     server = Thread(target=run)
